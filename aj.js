74dfd43833623457553c5c7b6419527c6a5b8bd1
akshay jain <123448064+AJAKSHAY77@users.noreply.github.com>
2023-07-28 11:32:27 +0530

    async method

---
+++ aj.js
@@ -0,0 +1,7 @@
+const start = document.getElementById("start")
+const aj = document.querySelector(".body")
+start.addEventListener("click",(e)=>{
+    aj.style.backgroundColor = "orange"
+     
+
+})
